33a304965b9ce0f0ecd7a9202ace53ccf271ff2d
Ruslan <54860448+heckfy88@users.noreply.github.com>
2024-12-01 14:46:39 +0300

    SB-3: настройка litellm_proxy + агенты и задачи

--- src/sb_codebreakers/crew.py
+++ src/sb_codebreakers/crew.py
@@ -1,62 +1,65 @@
-from crewai import Agent, Crew, Process, Task, LLM
+from crewai import Agent, Crew, Process, LLM, Task
 from crewai.project import CrewBase, agent, crew, task
 
-
 from src.sb_codebreakers.tools.git_search_tool import my_git_loader_tool
-
-# Uncomment the following line to use an example of a custom tool
-# from cb.tools.custom_tool import MyCustomTool
-
-# Check our tools documentations for more information on how to use them
-# from crewai_tools import SerperDevTool
-
-repo_path = "/Users/ruslanagaev/uni/codebreaker"
 
 
 @CrewBase
 class SbCodebreakersCrew:
-	"""Cb crew"""
+    """Cb crew"""
 
-	@agent
-	def retrieval_agent(self) -> Agent:
-		return Agent(
-			config=self.agents_config['retrieval_agent'],
-			tools = [my_git_loader_tool],
-			verbose=True,
-			llm=LLM(model="ollama/llama3.1", base_url="http://localhost:11434"),
-			task=self.retrieve_task
-		)
+    @agent
+    def retrieval_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['retrieval_agent'],
+            tools=[my_git_loader_tool],
+            verbose=True,
+            llm=LLM(
+                model="litellm_proxy/gigachat-custom-model",
+                base_url="http://localhost:4000",
+                # не нужно, но вдруг придется указывать разные ключи для каждого агента?
+                api_key="<API_KEY>"
+            ),
+            task=self.retrieve_task
+        )
 
-	@task
-	def retrieve_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['retrieve_task'],
-			human_input=True,
-		)
+    @agent
+    def fixing_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config['fixing_agent'],
+            verbose=True,
+            llm=LLM(
+                model="litellm_proxy/gigachat-custom-model",
+                base_url="http://localhost:4000",
+                api_key="<API_KEY>"
+            ),
+            task=self.fix_task
+        )
 
-	@agent
-	def counting_agent(self) -> Agent:
-		return Agent(
-			config=self.agents_config['counting_agent'],
-			verbose=True,
-			llm=LLM(model="ollama/llama3.1", base_url="http://localhost:11434"),
-			task=self.counting_task
-		)
+    @task
+    def retrieve_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['retrieve_task'],
+            human_input=True,
+        )
 
-	@task
-	def counting_task(self) -> Task:
-		return Task(
-			config=self.tasks_config['counting_task'],
-			human_input=True,
-		)
+    @task
+    def fix_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['fix_task'],
+            human_input=True,
+            output_file="diff.txt"
+        )
 
-	@crew
-	def crew(self) -> Crew:
-		"""Creates the Cb crew"""
-		return Crew(
-			agents=self.agents, # Automatically created by the @agent decorator
-			tasks=self.tasks, # Automatically created by the @task decorator
-			process=Process.sequential,
-			verbose=True,
-			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
-		)
+    @crew
+    def crew(self) -> Crew:
+        return Crew(
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,  # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True,
+            # желательно бы настроить, но у меня он пытается обратиться к OpenAI все равно
+            #planning=True,
+            #planning_llm
+            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+        )
